1e8fa966ee17c3631029e92cefdd7c1b7e6fcb8d
Yelyzaveta Herasymenko <elizaherasymenko@gmail.com>
2025-03-31 20:29:18 +0300

    Functions were added.

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def input_text_from_console():
+    pass
+
+def read_file_builtin(file_path):
+    pass
+
+def read_file_with_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def output_text_to_console(text):
+    pass
+
+def write_to_file_builtin(file_path, content):
+    pass
+
+def write_to_file_with_pandas(file_path, content):
+    pass
